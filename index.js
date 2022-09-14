17609c438aff04e914b7fd73081c826bb0e9ea4c
mikeson <mikeson.jnr@gmail.com>
2022-09-14 15:31:27 +0100

    Fix phone number validation ui + more information

--- index.js
+++ index.js
@@ -5,40 +5,45 @@ const email = document.getElementById("user_email");
 const phone_number = document.getElementById("phone_number");
 const password = document.getElementById("user_password");
 const password2 = document.getElementById("confirm_password");
+const small2 = document.getElementById("format");
 
 
 form.addEventListener("focusout", event => {
 
-  if(event.target==fName){
+  if (event.target == fName) {
     checkFirstName();
-  } else if (event.target == lName){
+  } else if (event.target == lName) {
     checkLastName();
-  } else if (event.target == email){
+  } else if (event.target == email) {
     checkEmail();
-  } else if (event.target == phone_number){
+  } else if (event.target == phone_number) {
     checkPhoneNumber();
+  } else if (event.target == password) {
+    checkPassword();
+  } else if (event.target == password2) {
+    checkPassword2();
   }
-  
-  
+
+
 
 })
 
 //Check First Name
-function checkFirstName(){
+function checkFirstName() {
   const fNameValue = fName.value.trim();
 
-  if(fNameValue === "") {
-		setErrorFor(fName, "First Name cannot be blank");
-	} else {
-		setSuccessFor(fName);
-	}
+  if (fNameValue === "") {
+    setErrorFor(fName, "First Name cannot be blank");
+  } else {
+    setSuccessFor(fName);
+  }
 
 }
 
 //Check Last Name
-function checkLastName(){
+function checkLastName() {
   const lNameValue = lName.value.trim();
-  if(lNameValue.length == 0){
+  if (lNameValue.length == 0) {
     removeSuccessFor(lName);
   } else {
     setSuccessFor(lName);
@@ -47,52 +52,63 @@ function checkLastName(){
 }
 
 //Check Email
-  function checkEmail(){
-    const emailValue = email.value.trim();
-    if(emailValue.length == 0){
-      setErrorFor(email, "Email cannot be blank");
-    } else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailValue)){
-      setSuccessFor(email);
-    } else {
-      setErrorFor(email, "Please enter a valid email address");
-    }
-  }
-
-  //Check Phone Number
-  function checkPhoneNumber(){
-    const phone_numberValue = phone_number.value.trim();;
-    if(phone_numberValue == 0){
-      removeSuccessFor(phone_number);
-    } else if (/^(?:\W*\d){11}\W*$/.test(phone_numberValue)){
-      setSuccessFor(phone_number);
-    } else {
-      setErrorFor(phone_number, "Please enter a valid phone number");
+function checkEmail() {
+  const emailValue = email.value.trim();
+  if (emailValue.length == 0) {
+    setErrorFor(email, "Email cannot be blank");
+  } else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailValue)) {
+    setSuccessFor(email);
+  } else {
+    setErrorFor(email, "Please enter a valid email address");
   }
 }
 
-
-
-const passwordValue = password.value.trim();
-const password2Value = password2.value.trim();
+//Check Phone Number
+function checkPhoneNumber() {
+  const phone_numberValue = phone_number.value.trim();;
+  if (phone_numberValue == 0) {
+    removeSuccessFor(phone_number);
+    removeErrorFor(phone_number);
+  } else if (/^(?:\W*\d){11}\W*$/.test(phone_numberValue)) {
+    setSuccessFor(phone_number);
+  } else {
+    setErrorFor(phone_number, "Please enter a valid phone number");
+  }
+}
 
 
 
 
 function setErrorFor(input, message) {
-	const formControl = input.parentElement;
-	const small = formControl.querySelector("small");
-	formControl.className = "input error";
-	small.innerText = message;
+  const formControl = input.parentElement;
+  const small = formControl.querySelector(".error-text");
+  formControl.className = "input error";
+  small.innerText = message;
+    if (input == phone_number){
+    small2.style.visibility = "hidden";
+  }
 }
 
 
 function setSuccessFor(input) {
-	const formControl = input.parentElement;
-	formControl.className = "input success";
+  const formControl = input.parentElement;
+  formControl.className = "input success";
+  if (input == phone_number){
+    small2.style.visibility = "hidden";
+  }
 }
 
-function removeSuccessFor(input){
-    const formControl = input.parentElement;
-    formControl.classList.remove("success");
+function removeSuccessFor(input) {
+  const formControl = input.parentElement;
+  formControl.classList.remove("success");
 
+}
+
+function removeErrorFor(input){
+  const formControl = input.parentElement;
+  formControl.classList.remove("error");
+  if (input == phone_number){
+    small2.style.visibility = "visible";
   }
+
+}
