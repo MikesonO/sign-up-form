9783dce5a3a5b9541f594ce3f19ce1e95fd6e38d
mikeson <mikeson.jnr@gmail.com>
2022-09-14 09:16:37 +0100

    Add global variables to index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const form = document.getElementById("input");
+const fName = document.getElementById("first_name");
+const lName = document.getElementById("last_name");
+const email = document.getElementById("user_email");
+const phone_number = document.getElementById("phone_number");
+const password = document.getElementById("user_password");
+const password2 = document.getElementById("confirm_password");
