9c0ca726ab62c2ad665e2326434a86915d23e12c
mikeson <mikeson.jnr@gmail.com>
2022-09-14 12:39:05 +0100

    Add ui error checks for first name and last name

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const form = document.getElementById("input");
+const form = document.getElementById("sign-up-form");
 const fName = document.getElementById("first_name");
 const lName = document.getElementById("last_name");
 const email = document.getElementById("user_email");
@@ -6,17 +6,98 @@ const phone_number = document.getElementById("phone_number");
 const password = document.getElementById("user_password");
 const password2 = document.getElementById("confirm_password");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
 
-  checkInputs();
-});
+// form.addEventListener("submit", event => {
+//   event.preventDefault();
+
+//   checkInputs();
+// });
+
+
+
+form.addEventListener("focusout", event => {
+
+  if(event.target==fName){
+    checkFirstName();
+  } else if (event.target == lName){
+    checkLastName();
+  }
+  
+  
+
+})
+
+//Check First Name
+function checkFirstName(){
+  const fNameValue = fName.value.trim();
+
+  if(fNameValue === "") {
+		setErrorFor(fName, "First Name cannot be blank");
+	} else {
+		setSuccessFor(fName);
+	}
+
+}
+
+//Check Last Name
+function checkLastName(){
+  const lNameValue = lName.value.trim();
+  if(lNameValue.length == 0){
+    removeSuccessFor(lName);
+  } else {
+    setSuccessFor(lName);
+  }
+
+}
+
+
+
+
+
+
+
 
 function checkInputs() {
-  const fNameValue = fName.value;
-  const lNameValue = lName.value;
-  const emailValue = email.value;
-  const phone_numberValue = phone_number.value;
-  const passwordValue = password.value;
-  const password2Value = password2.value;
+  const fNameValue = fName.value.trim();
+  const lNameValue = lName.value.trim();
+  const emailValue = email.value.trim();
+  const phone_numberValue = phone_number.value.trim();
+  const passwordValue = password.value.trim();
+  const password2Value = password2.value.trim();
+
+
+	if(fNameValue === "") {
+		setErrorFor(fName, "First Name cannot be blank");
+	} else {
+		setSuccessFor(fName);
+	}
+
+
+  if(lNameValue !== "") {
+    setSuccessFor(lName);
+  } else {
+    removeSuccess(lName)
+  }
+
 }
+
+
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector("small");
+	formControl.className = "input error";
+	small.innerText = message;
+}
+
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = "input success";
+}
+
+function removeSuccessFor(input){
+    const formControl = input.parentElement;
+    formControl.classList.remove("success");
+
+  }
